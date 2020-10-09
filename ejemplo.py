0b1938aa8510d4e9c1d3c085a34d6b396c15e81a
victorisabelruiz <victorisabelruiz@hotmail.com>
2020-10-09 12:42:57 +0200

    creamos el metodo __str__

--- ejemplo.py
+++ ejemplo.py
@@ -2,4 +2,8 @@ class Vecinos_Modelo():
     def __init__(self):
         print("Se crea un objeto de la clase el modelo Vecinos")
 
+    def __str__(self):
+        return "Probando el metodo __str__"
+
 model = Vecinos_Modelo()
+print(model)
